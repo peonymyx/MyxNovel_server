331fcb171fa6eacf5c44322e9085d52425a62bbb
peonymyx <peonymyx@gmail.com>
2024-01-29 21:34:24 +0700

    banking

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('Server running on http://localhost:3000');
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Request, Response } from 'express';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+   addToken(@Req() req: Request, @Res() res: Response) {
+    if(req.headers['secure-token'] != "05102004")
+    {
+      return res.status(500).json({
+        message:"Nạp tiền không thành công",
+      })
+    }
+    return res.status(200).json({
+      message: 'nạp tiền thành công successfully',
+    })
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
